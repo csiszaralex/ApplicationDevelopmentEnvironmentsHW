770bfcac8f239a8e556b95dede584202226c4174
Alex Csiszar <csiszaralex@gmail.com>
2024-03-25 22:45:50 +0100

    Adds User and Item entity

---
+++ backend/src/main/kotlin/hu/bme/auction/entity/Item.kt
@@ -0,0 +1,41 @@
+package hu.bme.auction.entity
+
+import jakarta.persistence.*
+import org.hibernate.proxy.HibernateProxy
+import java.time.OffsetDateTime
+
+@Entity
+@Table(name = "items")
+open class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    open var id: Long? = null
+
+    @Column(name = "title", nullable = false)
+    open var title: String? = null
+
+    @Enumerated
+    @Column(name = "quality")
+    open var quality: Quality? = null
+
+    @Column(name = "payed")
+    open var payed: Boolean? = null
+
+    @Column(name = "from")
+    open var from: OffsetDateTime? = null
+
+    @Column(name = "to")
+    open var to: OffsetDateTime? = null
+
+    @Column(name = "starting_bid", nullable = false)
+    open var startingBid: Int? = null
+
+    @ManyToOne(cascade = [CascadeType.ALL], optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    open var user: User? = null
+}
+
+enum class Quality {
+    NEW, LIKENEW, USED
+}
---
+++ backend/src/main/kotlin/hu/bme/auction/entity/User.kt
@@ -0,0 +1,38 @@
+package hu.bme.auction.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+open class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    open var id: Long? = null
+
+    @Column(name = "name", nullable = false)
+    open var name: String? = null
+
+    @Column(name = "full_name", nullable = false)
+    open var fullName: String? = null
+
+    @Column(name = "email", nullable = false, unique = true)
+    open var email: String? = null
+
+    @Column(name = "salt")
+    open var salt: String? = null
+
+    @Column(name = "password")
+    open var password: String? = null
+
+    @Enumerated
+    @Column(name = "role")
+    open var role: Role? = null
+
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
+    open var items: MutableSet<Item> = mutableSetOf()
+}
+
+enum class Role {
+    ADMIN, USER
+}
