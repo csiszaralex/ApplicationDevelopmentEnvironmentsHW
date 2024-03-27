5476cb33e05f0c2912bd4ab79c01a1cb82c8ffe6
Alex Csiszar <csiszaralex@gmail.com>
2024-03-27 15:53:09 +0100

    bugfix remove spring security

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.h2database:h2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- backend/src/main/kotlin/hu/bme/auction/AuctionApplication.kt
+++ backend/src/main/kotlin/hu/bme/auction/AuctionApplication.kt
@@ -1,17 +1,10 @@
 package hu.bme.auction
 
-import hu.bme.auction.repository.UserRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
 @SpringBootApplication
-class AuctionApplication(val userRepository: UserRepository){
-	@GetMapping("/")
-	fun hello() = userRepository.count().toString()
-}
+class AuctionApplication()
 
 fun main(args: Array<String>) {
 	runApplication<AuctionApplication>(*args)
