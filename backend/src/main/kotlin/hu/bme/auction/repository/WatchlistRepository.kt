4ec82fcb88a712d6ca4effcf5f1e347ac034a487
Alex Csiszar <csiszaralex@gmail.com>
2024-03-27 15:53:46 +0100

    remove watchlist and feedback classes and repositories

--- backend/src/main/kotlin/hu/bme/auction/repository/WatchlistRepository.kt
+++
@@ -1,9 +0,0 @@
-package hu.bme.auction.repository;
-
-import hu.bme.auction.entity.Watchlist
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface WatchlistRepository : JpaRepository<Watchlist, Long> {
-}
