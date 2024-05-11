c693611a3ba744c2fa018ea388fec6a23813fb46
Alex Csiszár <csiszaralex@gmail.com>
2024-05-12 00:06:50 +0200

    Another import issue in BidRepository

--- backend/src/main/kotlin/hu/bme/auction/dao/BidRepository.kt
+++ backend/src/main/kotlin/hu/bme/auction/dao/BidRepository.kt
@@ -2,7 +2,6 @@ package hu.bme.auction.dao
 
 import hu.bme.auction.entity.Bid
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
