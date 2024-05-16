3220d233f96c09da2ffef12a9d5a0a7ac4eaef99
Frigyes309 <frigyes.smuk@gmail.com>
2024-05-16 17:05:03 +0200

    CategoryControllerTest created

--- backend/src/test/kotlin/hu/bme/auction/controller/CategoryControllerTest.kt
+++ backend/src/test/kotlin/hu/bme/auction/controller/CategoryControllerTest.kt
@@ -9,5 +9,45 @@ import org.springframework.web.bind.annotation.*
 
 @SpringBootTest
 class CategoryController(val categoryService: CategoryService) {
-    
+    @Test
+    fun testCreateCategory() {
+        val category = Category()
+        val categoryService = CategoryService()
+        val categoryController = CategoryController(categoryService)
+        val responseEntity = categoryController.createCategory(category)
+        assertEquals(HttpStatus.CREATED, responseEntity.statusCode)
+    }
+
+    @Test
+    fun testGetCategories() {
+        val categoryService = CategoryService()
+        val categoryController = CategoryController(categoryService)
+        val responseEntity = categoryController.getCategories()
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    @Test
+    fun testGetCategory() {
+        val categoryService = CategoryService()
+        val categoryController = CategoryController(categoryService)
+        val responseEntity = categoryController.getCategory(1)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    @Test
+    fun testDeleteCategory() {
+        val categoryService = CategoryService()
+        val categoryController = CategoryController(categoryService)
+        val responseEntity = categoryController.deleteCategory(1)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    @Test
+    fun testUpdateCategory() {
+        val categoryService = CategoryService()
+        val categoryController = CategoryController(categoryService)
+        val category = Category()
+        val responseEntity = categoryController.updateCategory(1, category)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
 }
