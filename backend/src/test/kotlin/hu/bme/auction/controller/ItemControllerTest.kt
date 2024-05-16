0719a7ca49ea4044f19d6d74a50d5dee4ca81bf9
Frigyes309 <frigyes.smuk@gmail.com>
2024-05-16 18:17:23 +0200

    ItemControllerTest commented

--- backend/src/test/kotlin/hu/bme/auction/controller/ItemControllerTest.kt
+++ backend/src/test/kotlin/hu/bme/auction/controller/ItemControllerTest.kt
@@ -9,6 +9,69 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@SpringBootTest
 class ItemController() {
-    
+    /*
+        * Create an item with the given item
+        * @param item to create
+        * @return ResponseEntity with status code 201
+     */
+    @Test
+    fun testCreateItem() {
+        val item = Item()
+        val itemService = ItemService()
+        val itemController = ItemController(itemService)
+        val createItemDto = CreateItemDto()
+        val responseEntity = itemController.createItem(createItemDto)
+        assertEquals(HttpStatus.CREATED, responseEntity.statusCode)
+    }
+
+    /*
+        * Get all items
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testGetItems() {
+        val itemService = ItemService()
+        val itemController = ItemController(itemService)
+        val responseEntity = itemController.getItems()
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Get an item
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testGetItem() {
+        val itemService = ItemService()
+        val itemController = ItemController(itemService)
+        val responseEntity = itemController.getItem(1)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Delete an item
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testDeleteItem() {
+        val itemService = ItemService()
+        val itemController = ItemController(itemService)
+        val responseEntity = itemController.delete(1)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Update an item
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testUpdateItem() {
+        val itemService = ItemService()
+        val itemController = ItemController(itemService)
+        val createItemDto = CreateItemDto()
+        val responseEntity = itemController.updateItem(1, createItemDto)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
 }
