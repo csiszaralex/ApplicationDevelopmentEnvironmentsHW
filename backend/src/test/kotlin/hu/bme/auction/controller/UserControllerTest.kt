f4fe4bc88c43c295ab6cdd47398bd2f285a918ae
Frigyes309 <frigyes.smuk@gmail.com>
2024-05-16 18:29:24 +0200

    UserControllerTest created and commented

--- backend/src/test/kotlin/hu/bme/auction/controller/UserControllerTest.kt
+++ backend/src/test/kotlin/hu/bme/auction/controller/UserControllerTest.kt
@@ -10,6 +10,85 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@SpringBootTest
 class UserController(private val userService: UserService) {
+    /*
+        * Register a user with the given user
+        * @param user to register
+        * @return ResponseEntity with status code 201
+     */
+    @Test
+    fun testRegisterUser() {
+        val user = User()
+        val userService = UserService()
+        val userController = UserController(userService)
+        val registerUserDto = RegisterUserDto()
+        val responseEntity = userController.registerUser(registerUserDto)
+        assertEquals(HttpStatus.CREATED, responseEntity.statusCode)
+    }
+
+    /*
+        * Login a user with the given user
+        * @param user to login
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testLoginUser() {
+        val user = User()
+        val userService = UserService()
+        val userController = UserController(userService)
+        val loginUserDto = LoginUserDto()
+        val responseEntity = userController.loginUser(loginUserDto)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Get all users
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testGetUsers() {
+        val userService = UserService()
+        val userController = UserController(userService)
+        val responseEntity = userController.getUsers()
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Get a user
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testGetUser() {
+        val userService = UserService()
+        val userController = UserController(userService)
+        val responseEntity = userController.getUser(1)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Delete a user
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testDeleteUser() {
+        val userService = UserService()
+        val userController = UserController(userService)
+        val responseEntity = userController.deleteUser(1)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
+
+    /*
+        * Update a user
+        * @return ResponseEntity with status code 200
+     */
+    @Test
+    fun testUpdateUser() {
+        val userService = UserService()
+        val userController = UserController(userService)
+        val user = User()
+        val responseEntity = userController.updateUser(1, user)
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
+    }
 
 }
