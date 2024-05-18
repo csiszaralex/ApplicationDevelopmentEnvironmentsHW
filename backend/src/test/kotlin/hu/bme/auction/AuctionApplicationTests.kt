317d98780173ec69d39b2ed61badb7a60c9b89b1
Alex Csiszár <csiszaralex@gmail.com>
2024-05-18 13:30:29 +0200

    Adds test config and set its config for tests

--- backend/src/test/kotlin/hu/bme/auction/AuctionApplicationTests.kt
+++ backend/src/test/kotlin/hu/bme/auction/AuctionApplicationTests.kt
@@ -2,8 +2,10 @@ package hu.bme.auction
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:application-test.properties"])
 class AuctionApplicationTests {
 
 	@Test
