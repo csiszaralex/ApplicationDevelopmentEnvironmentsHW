ff6949217d8bd8a9dea1db6458eb295541f4d36d
David Varga <varga.i.david23@gmail.com>
2024-05-19 17:06:57 +0300

    Remove application icon
    Quick fixes on the frontend. Last commit saved errorous code

--- Bidder/main.cpp
+++ Bidder/main.cpp
@@ -18,8 +18,6 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    QWindow::setIcon();
-
     // Inicializaljuk az adatokat:
     istantiateLists(app);
 
